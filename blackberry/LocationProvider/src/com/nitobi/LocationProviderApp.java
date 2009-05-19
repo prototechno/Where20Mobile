9f3e3adee402e283938e492d6416fc2708b1baa4
Dave Johnson <davejohnson@dave-johnsons-macbook-pro.local>
2009-05-19 16:09:29 -0700

    stuff

--- blackberry/LocationProvider/src/com/nitobi/LocationProviderApp.java
+++ blackberry/LocationProvider/src/com/nitobi/LocationProviderApp.java
@@ -9,13 +9,6 @@ import net.rim.device.api.ui.container.MainScreen;
 
 import net.rim.device.api.ui.component.TextField;
 
-/*
-import net.rim.device.api.system.Application;
-import net.rim.device.api.system.*;
-import net.rim.device.api.system.Alert;
-import net.rim.blackberry.api.invoke.*;
-*/
-
 public class LocationProviderApp extends UiApplication implements LocationListener {
 
     public static void main(String[] args) {
@@ -43,24 +36,11 @@ public class LocationProviderApp extends UiApplication implements LocationListen
         	_locationProvider = LocationProvider.getInstance(null);
         	_locationProvider.setLocationListener(this, 1, 1, 1);
         } catch(Exception ex) {
-        	System.out.println("########### could not get location provider");
         }
-    }
-
-    private void onClose() {
-    	_locationProvider.reset();
-        _locationProvider.setLocationListener(null, -1, -1, -1);
-    }
-    
-    private void updateLocation(double lat, double lng)
-    {
-    	System.out.println("######### updateLocation");
-    	_textField.setText(String.valueOf(lat) + ", " + String.valueOf(lng));
     }
     
 	public void locationUpdated(LocationProvider provider, Location location) {
 		// TODO Auto-generated method stub
-		System.out.println("######### locationUpdated");
 		if(location.isValid())
         {
             double longitude = location.getQualifiedCoordinates().getLongitude();
@@ -69,7 +49,7 @@ public class LocationProviderApp extends UiApplication implements LocationListen
             synchronized(this)
             {
                 // Now set this somewhere and make it available to the JavaScript
-            	LocationProviderApp.this.updateLocation(latitude, longitude);
+            	LocationProviderApp.this._textField.setText(String.valueOf(latitude) + ", " + String.valueOf(longitude));
             }
         }
 	}
@@ -78,23 +58,4 @@ public class LocationProviderApp extends UiApplication implements LocationListen
 		// TODO Auto-generated method stub
 		
 	}
-     
-     /*
-    private void updateLocation(double lat, double lng, float speed, float alt, float head)
-    {
-        System.out.println("**** update location: " + String.valueOf(lat) + ", " + String.valueOf(lng));
-        this._lat = lat;
-        this._lng = lng;
-        this._locationReady = true;
-    }
-      */
-     
-     /*
-if (_getLocation && _locationReady) {
-         cookie += "geolocation: {lat:" + String.valueOf(this._lat) + ",lng:" + String.valueOf(this._lng)+"},";
-         _locationReady = false;
-             _locationProvider.reset();
-             _locationProvider.setLocationListener(null, -1, -1, -1);
-       }
-      */
 }
